004922e0e40bd498d4bbf566d19533f833aaf5b7
GerardoNR <gerardo.noe07@gmail.com>
2026-08-14 19:08:50 -0600

    mobile: configurar cliente base de API

---
+++ mobile/lib/services/api_service.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+
+import '../config/api_config.dart';
+
+class ApiService {
+  ApiService()
+    : _dio = Dio(
+        BaseOptions(
+          baseUrl: ApiConfig.baseUrl,
+          connectTimeout: const Duration(seconds: 10),
+          receiveTimeout: const Duration(seconds: 30),
+          headers: const {
+            Headers.contentTypeHeader: Headers.jsonContentType,
+            Headers.acceptHeader: Headers.jsonContentType,
+          },
+        ),
+      );
+
+  final Dio _dio;
+
+  Future<Response<T>> get<T>(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+  }) {
+    return _dio.get<T>(path, queryParameters: queryParameters);
+  }
+
+  Future<Response<T>> post<T>(String path, {Object? data}) {
+    return _dio.post<T>(path, data: data);
+  }
+
+  Future<Response<T>> patch<T>(String path, {Object? data}) {
+    return _dio.patch<T>(path, data: data);
+  }
+}
